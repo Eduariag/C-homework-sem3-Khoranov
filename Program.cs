826e186f03a06a2a86f1d2a5d5c8fc7786098039
Eduariag <uariagsmart@gmail.com>
2022-12-14 23:39:03 +0300

    все задачи решены

--- Program.cs
+++ Program.cs
@@ -50,3 +50,26 @@ Console.WriteLine("The cubes of numbers:  ");
 Cubes (num);
 */
 
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+/*
+Console.WriteLine("Input number:  ");
+string number=Console.ReadLine();
+int len= number.Length;
+
+if(len==5)
+{
+    if(number [0] == number [4] && number [1] == number [3])
+    {
+        Console.WriteLine ($"{number} is Polindrom");
+    }
+    else
+    {
+         Console.WriteLine ($"{number} isn't Polindrom");
+    }
+}
+else
+{
+    Console.WriteLine($"Mistake: {number} isn't five-digit");
+}
+*/
